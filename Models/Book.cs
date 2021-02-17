a3534795859575e33bfade1d9c571b97252c1ca2
joshcard <66044735+joshcard@users.noreply.github.com>
2021-02-16 21:34:28 -0700

    Regex might work

--- Models/Book.cs
+++ Models/Book.cs
@@ -24,13 +24,13 @@ namespace ItMightBeAmazon.Models
         public string Publisher { get; set; }
 
         [Required]
-        [RegularExpression("^(?:ISBN(?:-1[03])?:?●)?(?=[0-9X]{10}$|(?=(?:[0-9]+[-●]){3})[-●0 - 9X]{13}$|97[89] [0-9]{10}$| (?= (?:[0 - 9] +[-●]){ 4})[-●0-9]{ 17}$)(?: 97[89][-●] ?)?[0 - 9]{ 1,5}[-●]?[0 - 9] +[-●]?[0 - 9] +[-●]?[0 - 9X]$")]
+        [RegularExpression(@"\d{3}-?\d{10}")]
         public string ISBN { get; set; }
 
         [Required]
         public string Classification { get; set; }
 
         [Required]
-        public float Price { get; set; }
+        public double Price { get; set; }
     }
 }
